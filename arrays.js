e4deeebd3c3b552f25ac09b27343f425f1811707
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 18:32:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array,element){
-  return newArray=[array, ...element]
+  newArray=[array, ...element];
+  return newArray;
 }
