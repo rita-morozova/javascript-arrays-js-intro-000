df4f9848782e1263f39b4b75effd7355b630b06a
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 18:25:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOgArray(array, element){
   newArray = array.unsift(element);
   return newArray;
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+ return array.unsift(element);
+}
