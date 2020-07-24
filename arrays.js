d27eb10ed82cae4d32bafe73666ad7fa55b78de7
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 18:24:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOgArray(array, element){
+  newArray = array.unsift(element);
+  return newArray;
+}
